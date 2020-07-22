ab1b0dd194dd1652fedad9e1b614278a2540f2f6
StijnMaris <stijn.maris@me.com>
2020-07-23 01:11:43 +0200

    added a pistol

--- Source/CSNO/Public/CSNOCharacter.h
+++ Source/CSNO/Public/CSNOCharacter.h
@@ -21,125 +21,125 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnPlantedBomb, AActor*, Planter, A
 
 UCLASS(config=Game)
 class ACSNOCharacter : public ACharacter {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
-    ACSNOCharacter();
+	ACSNOCharacter();
 
-    virtual void Tick(float DeltaTime) override;
+	virtual void Tick(float DeltaTime) override;
 
-    UFUNCTION(BlueprintCallable, Category = "Game|Weapon")
-    FRotator GetAimOffsets() const;
+	UFUNCTION(BlueprintCallable, Category = "Game|Weapon")
+	FRotator GetAimOffsets() const;
 
-    void SetWalking(bool bNewWalking);
+	void SetWalking(bool bNewWalking);
 
-    /** AnimMontage to play each time we fire */
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
-    UAnimMontage* FireAnimation;
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	UAnimMontage* FireAnimation;
 
-    /** Returns Mesh1P subobject **/
-    FORCEINLINE class USkeletalMeshComponent* GetMeshFP() const { return MeshFP; }
+	/** Returns Mesh1P subobject **/
+	FORCEINLINE class USkeletalMeshComponent* GetMeshFP() const { return MeshFP; }
 
-    /** Returns FirstPersonCameraComponent subobject **/
-    FORCEINLINE class UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
+	/** Returns FirstPersonCameraComponent subobject **/
+	FORCEINLINE class UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
 
-    FORCEINLINE class UCSNOHealthComponent* GetHealthComponent() const { return HealthComponent; }
+	FORCEINLINE class UCSNOHealthComponent* GetHealthComponent() const { return HealthComponent; }
 
-    FORCEINLINE class UCSNOArmourComponent* GetArmourComponent() const { return ArmourComponent; }
+	FORCEINLINE class UCSNOArmourComponent* GetArmourComponent() const { return ArmourComponent; }
 
-    FORCEINLINE class UCSNOInventoryComponent* GetInventoryComponent() const { return InventoryComponent; }
+	FORCEINLINE class UCSNOInventoryComponent* GetInventoryComponent() const { return InventoryComponent; }
 
-    FName GetWeaponAttachSocketName() const;
+	FName GetWeaponAttachSocketName() const;
 
-    UFUNCTION()
-    void SetIsInBombPlantingSite(bool bIsInPlantingSite) {
-        bIsInBombPlantingSite = bIsInPlantingSite;
-    }
+	UFUNCTION()
+	void SetIsInBombPlantingSite(bool bIsInPlantingSite) {
+		bIsInBombPlantingSite = bIsInPlantingSite;
+	}
 
-    UFUNCTION()
-    FORCEINLINE bool IsInBombPlantingSite() const {
-        return bIsInBombPlantingSite;
-    }
+	UFUNCTION()
+	FORCEINLINE bool IsInBombPlantingSite() const {
+		return bIsInBombPlantingSite;
+	}
 
-    UPROPERTY(BlueprintAssignable, Category = "Events")
-    FOnPlantedBomb OnPlantedBomb;
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnPlantedBomb OnPlantedBomb;
 
-    UPROPERTY()
-    ACSNODefusableBomb* DefusableBomb;
+	UPROPERTY()
+	ACSNODefusableBomb* DefusableBomb;
+
+	//var
+	UPROPERTY(Transient, Replicated, BlueprintReadOnly)
+	bool bWantsToWalk;
 
 protected:
-    virtual void BeginPlay() override;
+	virtual void BeginPlay() override;
 
-    //input
+	//input
 
-    void StartFire();
+	void StartFire();
 
-    void StopFire();
+	void StopFire();
 
-    void Reload();
+	void Reload();
 
-    void BeginCrouch();
+	void BeginCrouch();
 
-    void EndCrouch();
+	void EndCrouch();
 
-    void BeginWalk();
+	void BeginWalk();
 
-    void EndWalk();
+	void EndWalk();
 
-    void AlternateFire();
+	void AlternateFire();
 
-    void Action();
+	void Action();
 
-    void StopAction();
+	void StopAction();
 
-    void ChangeToInventorySlot1();
-    void ChangeToInventorySlot2();
-    void ChangeToInventorySlot3();
-    void ChangeToInventorySlot4();
-    void ChangeToInventorySlot5();
+	void ChangeToInventorySlot1();
+	void ChangeToInventorySlot2();
+	void ChangeToInventorySlot3();
+	void ChangeToInventorySlot4();
+	void ChangeToInventorySlot5();
 
-    /** Handles moving forward/backward */
-    void MoveForward(float Val);
+	/** Handles moving forward/backward */
+	void MoveForward(float Val);
 
-    /** Handles stafing movement, left and right */
-    void MoveRight(float Val);
+	/** Handles stafing movement, left and right */
+	void MoveRight(float Val);
 
-    // APawn interface
-    virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
+	// APawn interface
+	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
 
-    UFUNCTION()
-    void OnHealthChanged(UCSNOHealthComponent* HealthComp, float Health, float HealthDelta,
-                         const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+	UFUNCTION()
+	void OnHealthChanged(UCSNOHealthComponent* HealthComp, float Health, float HealthDelta,
+	                     const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
 
-    UFUNCTION()
-    void OnDied(bool bIsDead, AActor* VictimActor, AActor* KillerActor, APlayerState* KillerPlayerState);
+	UFUNCTION()
+	void OnDied(bool bIsDead, AActor* VictimActor, AActor* KillerActor, APlayerState* KillerPlayerState);
 
-    UFUNCTION(reliable, server, WithValidation)
-    void ServerSetWalking(bool bNewWalking);
-
-    //var
-    UPROPERTY(Transient, Replicated)
-    bool bWantsToWalk;
+	UFUNCTION(reliable, server, WithValidation)
+	void ServerSetWalking(bool bNewWalking);
 
 private:
-    //var
-    /** Pawn mesh: 1st person view (arms; seen only by self) */
-    UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
-    class USkeletalMeshComponent* MeshFP;
+	//var
+	/** Pawn mesh: 1st person view (arms; seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+	class USkeletalMeshComponent* MeshFP;
 
-    /** First person camera */
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
-    UCameraComponent* FirstPersonCameraComponent;
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FirstPersonCameraComponent;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-    UCSNOHealthComponent* HealthComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UCSNOHealthComponent* HealthComponent;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-    UCSNOArmourComponent* ArmourComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UCSNOArmourComponent* ArmourComponent;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-    UCSNOInventoryComponent* InventoryComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UCSNOInventoryComponent* InventoryComponent;
 
-    UPROPERTY()
-    bool bIsInBombPlantingSite;
+	UPROPERTY()
+	bool bIsInBombPlantingSite;
 };
